82c2dd7c0d85dcd647eb89bfb4d5b5681e503975
ota <y.ohta.z3@future.co.jp>
2020-08-05 13:36:33 +0900

    Add VisitorKeys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./parser"
 import type * as AST from "./ast"
 import { getStaticYAMLValue } from "./utils"
+import { KEYS } from "./visitor-keys"
 
 export { AST, getStaticYAMLValue }
+export const VisitorKeys = KEYS
