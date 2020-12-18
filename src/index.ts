07cd973f05fecb2cc73603843464c94f80c170d0
Yosuke Ota <otameshiyo23@gmail.com>
2020-12-18 15:24:57 +0900

    Add traverseNodes and parseYAML (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-export * from "./parser"
+import { parseForESLint, ParseError } from "./parser"
 import type * as AST from "./ast"
+import { traverseNodes } from "./traverse"
 import { getStaticYAMLValue } from "./utils"
 import { KEYS } from "./visitor-keys"
 
-export { AST, getStaticYAMLValue }
+export { AST, ParseError }
+
+// parser
+export { parseForESLint }
+// Keys
 // eslint-disable-next-line @typescript-eslint/naming-convention -- ignore
 export const VisitorKeys = KEYS
+
+// tools
+export { traverseNodes, getStaticYAMLValue }
+
+/**
+ * Parse YAML source code
+ */
+export function parseYAML(code: string, _options?: any): AST.YAMLProgram {
+    return parseForESLint(code).ast
+}
