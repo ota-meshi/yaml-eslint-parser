89184575c5721b3d53d50763c888c587f68c61ee
ota-meshi <otameshiyo23@gmail.com>
2023-05-09 09:48:49 +0900

    feat: export meta object

---
+++ src/meta.ts
@@ -0,0 +1,5 @@
+// IMPORTANT!
+// This file has been automatically generated,
+// in order to update its content execute "npm run build:meta"
+export const name = "yaml-eslint-parser" as const;
+export const version = "1.2.1" as const;
