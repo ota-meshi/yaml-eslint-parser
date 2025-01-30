76f4b83a53d8e1f57f12e87fe84ef339c4550878
Francois Leurent <131.code@leurent.email>
2025-01-30 23:31:44 +0100

    Options should come from ParseOptions & DocumentOptions

--- src/options.ts
+++ src/options.ts
@@ -1,14 +1,21 @@
-import type { DocumentOptions } from "yaml";
+import type { DocumentOptions, ParseOptions } from "yaml";
 
 /**
  * ESLint parserOptions to `yaml`'s Composer options.
  */
-export function parserOptionsToYAMLOption(options: any): DocumentOptions {
+export function parserOptionsToYAMLOption(options: any): DocumentOptions & ParseOptions {
   if (!options) {
     return {};
   }
-  const result: DocumentOptions = {};
+  const result: DocumentOptions & ParseOptions = {};
   const version = options.defaultYAMLVersion;
+
+  const fwd = ["uniqueKeys", "strict"] as const;
+
+  for(let opt of fwd)
+    if(opt in options)
+      result[opt] = options[opt];
+
   if (typeof version === "string" || typeof version === "number") {
     const sVer = String(version);
     if (sVer === "1.2" || sVer === "1.1") {
