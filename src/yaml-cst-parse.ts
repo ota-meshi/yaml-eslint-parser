833aa70d8f0576f3a19aeccd2d5a28cefb323863
Yosuke Ota <otameshiyo23@gmail.com>
2025-12-05 11:26:26 +0900

    Refactor to use `composer.compose` (#252)
    * refactor to use `composer.compose`
    
    * update

--- src/yaml-cst-parse.ts
+++ src/yaml-cst-parse.ts
@@ -17,31 +17,17 @@ export function parseAllDocsToCST(ctx: Context): ParsedCSTDocs {
     keepSourceTokens: true,
     lineCounter,
   });
-  const cstNodes: CST.Token[] = [];
+  const cstNodes: CST.Token[] = [...parser.parse(ctx.code)];
   const nodes: Document.Parsed[] = [];
-
-  /**
-   * Process for Document
-   */
-  function processDoc(node: Document.Parsed) {
-    for (const error of node.errors) {
+  for (const doc of composer.compose(cstNodes)) {
+    for (const error of doc.errors) {
       throw ctx.throwError(error.message, error.pos[0]);
     }
     // ignore warns
     // for (const error of doc.warnings) {
     //     throw ctx.throwError(error.message, error.pos[0])
     // }
-    nodes.push(node);
-  }
-
-  for (const cst of parser.parse(ctx.code)) {
-    cstNodes.push(cst);
-    for (const doc of composer.next(cst)) {
-      processDoc(doc);
-    }
-  }
-  for (const doc of composer.end()) {
-    processDoc(doc);
+    nodes.push(doc);
   }
 
   return { nodes, cstNodes, streamInfo: composer.streamInfo() };
